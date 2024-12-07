3063c9fcfb035b6e79fed98a2eefa32cf2b7ccf4
Haibm <haibm@avadagroup.com>
2024-12-07 13:39:29 +0700

    add BE

---
+++ be/src/apis/order.api.js
@@ -0,0 +1,15 @@
+const orderApi = require('express').Router();
+const orderController = require('../controllers/order.controller');
+
+// api: lấy danh sách đơn hàng
+orderApi.get('/list', orderController.getOrderList);
+
+// api: lấy chi tiết 1 đơn hàng
+orderApi.get('/', orderController.getOrderDetails);
+
+// api: tạo 1 đơn hàng (tách nhiều sản phẩm ra mỗi sp 1 đơn)
+orderApi.post('/', orderController.postCreateOrder);
+
+orderApi.post('/signature', orderController.postCreateOrderSignature);
+
+module.exports = orderApi;
